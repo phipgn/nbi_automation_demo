80bb66f13ee8abaa9ccc32db66f3bceda6c5e4ca
Phi <phipgn@gmail.com>
2023-08-19 22:21:25 +0700

    update

--- src/test/java/com/giaphi/nbi/helpers/DriverHelper.java
+++ src/test/java/com/giaphi/nbi/helpers/DriverHelper.java
@@ -2,11 +2,14 @@ package com.giaphi.nbi.helpers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverHelper {
     public static WebDriver getDriver() {
         var path = System.getProperty("user.dir");
+        ChromeOptions options = new ChromeOptions();
         System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver.exe");
-        return new ChromeDriver();
+        options.addArguments("--remote-allow-origins=*");
+        return new ChromeDriver(options);
     }
 }
